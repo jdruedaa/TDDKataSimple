2c953daf3ff0c11b420b266fc86ba916029d13db
aromero45 <alex4543@hotmail.com>
2021-03-23 23:34:41 -0500

    It 2 Tests contar, minimo 1, maximo 1 y promedio 1 (Rojo)

--- ProcesadorTest.py
+++ ProcesadorTest.py
@@ -16,5 +16,18 @@ class ProcesadorTest(TestCase):
     def test_promedio(self):
         self.assertEqual(Procesador().promedio(""), [], "Cadena Vacía")
 
+    def test_contarunnumero(self):
+        self.assertEqual(Procesador().contar("1"), [1], "Cadena 1")
+
+    def test_minimounnumero(self):
+        self.assertEqual(Procesador().minimo("1"), [1,1], "Cadena 1")
+
+    def test_maximounnumero(self):
+        self.assertEqual(Procesador().maximo("1"), [1,1,1], "Cadena 1")
+
+    def test_promediounnumero(self):
+        self.assertEqual(Procesador().promedio("1"), [1,1,1,1], "Cadena 1")
+
+
 
 
