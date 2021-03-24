273cd78e19c64b9e6b569698b0b898190c6ad13a
JuanR <jd.ruedaa@uniandes.edu.co>
2021-03-24 00:45:50 -0500

    It 4 All tests (Refactor)

--- Procesador.py
+++ Procesador.py
@@ -4,45 +4,25 @@ class Procesador:
         if cadena == "":
             return 0
         valores = list(map(int, cadena.split(',')))
-        if len(valores) == 1:
-            return 1
-        elif len(valores) == 2:
-            return 2
-        else:
-            return len(valores)
+        return len(valores)
 
     def minimo(self, cadena):
         if cadena == "":
             return 0
         valores = list(map(int, cadena.split(',')))
-        if len(valores) == 1:
-            return int(cadena)
-        elif len(valores) == 2:
-            return min(valores)
-        else:
-            return min(valores)
+        return min(valores)
 
     def maximo(self, cadena):
         if cadena == "":
             return 0
         valores = list(map(int, cadena.split(',')))
-        if len(valores) == 1:
-            return int(cadena)
-        elif len(valores) == 2:
-            return max(valores)
-        else:
-            return max(valores)
+        return max(valores)
 
     def promedio(self, cadena):
         if cadena == "":
             return 0
         valores = list(map(int, cadena.split(',')))
-        if len(valores) == 1:
-            return int(cadena)
-        elif len(valores) == 2:
-            return sum(valores)/2
-        else:
-            return sum(valores)/len(valores)
+        return sum(valores)/len(valores)
 
     def respuesta(self, cadena):
         if cadena == "":
