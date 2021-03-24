2e2f2ba63289f9a34a668103f04b605aecd58d9b
JuanR <jd.ruedaa@uniandes.edu.co>
2021-03-23 22:53:23 -0500

    It 1 Tests contar, minimo, maximo y promedio (Rojo)

--- Procesador.py
+++ Procesador.py
@@ -1,3 +1,9 @@
 class Procesador:
     def contar(self,cadena):
         pass
+    def minimo(self,cadena):
+        pass
+    def maximo(self,cadena):
+        pass
+    def promedio(self,cadena):
+        pass
--- ProcesadorTest.py
+++ ProcesadorTest.py
@@ -5,5 +5,11 @@ from Procesador import Procesador
 
 class ProcesadorTest(TestCase):
     def test_contar(self):
-        self.assertEqual(Procesador().contar(""), [0,0,0,0], "Cadena Vacía")
+        self.assertEqual(Procesador().contar(""), [], "Cadena Vacía")
+    def test_minimo(self):
+        self.assertEqual(Procesador().minimo(""), [], "Cadena Vacía")
+    def test_maximo(self):
+        self.assertEqual(Procesador().maximo(""), [], "Cadena Vacía")
+    def test_promedio(self):
+        self.assertEqual(Procesador().promedio(""), [], "Cadena Vacía")
 
