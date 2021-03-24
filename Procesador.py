af54d5bf870b8319216f8c6db90688ee7d0634b2
JuanR <jd.ruedaa@uniandes.edu.co>
2021-03-24 00:36:14 -0500

    It 3 Tests con 2 números (Refactor)

--- Procesador.py
+++ Procesador.py
@@ -3,30 +3,38 @@ class Procesador:
     def contar(self, cadena):
         if cadena == "":
             return 0
-        else:
-            valores = list(map(int,cadena.split(',')))
-            return len(valores)
+        valores = list(map(int, cadena.split(',')))
+        if len(valores) == 1:
+            return 1
+        elif len(valores) == 2:
+            return 2
 
     def minimo(self, cadena):
         if cadena == "":
             return 0
-        else:
-            valores = list(map(int, cadena.split(',')))
+        valores = list(map(int, cadena.split(',')))
+        if len(valores) == 1:
+            return int(cadena)
+        elif len(valores) == 2:
             return min(valores)
 
     def maximo(self, cadena):
         if cadena == "":
             return 0
-        else:
-            valores = list(map(int, cadena.split(',')))
+        valores = list(map(int, cadena.split(',')))
+        if len(valores) == 1:
+            return int(cadena)
+        elif len(valores) == 2:
             return max(valores)
 
     def promedio(self, cadena):
         if cadena == "":
             return 0
-        else:
-            valores = list(map(int, cadena.split(',')))
-            return sum(valores)/len(valores)
+        valores = list(map(int, cadena.split(',')))
+        if len(valores) == 1:
+            return int(cadena)
+        elif len(valores) == 2:
+            return sum(valores)/2
 
     def respuesta(self, cadena):
         if cadena == "":
